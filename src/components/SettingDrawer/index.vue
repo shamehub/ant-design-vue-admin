103b3cb83f5cc5a634d14f3c0f93d0b048333753
shamehub <wxk.hub@gmail.com>
2020-08-22 01:19:41 +0800

    1

--- src/components/SettingDrawer/index.vue
+++ src/components/SettingDrawer/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <a-drawer
-      title="Basic Drawer"
       placement="right"
       :closable="false"
       :visible="visible"
@@ -13,9 +12,28 @@
           <a-icon :type="visible ? 'close' : 'setting'"></a-icon>
         </div>
       </template>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      <div>
+        <template>
+          <div>
+            <h2>整体风格定制</h2>
+            <a-radio-group
+              :value="$route.query.navTheme || 'dark'"
+              @change="e => handleSettingChange('navTheme', e.target.value)"
+            >
+              <a-radio value="dark">黑色</a-radio>
+              <a-radio value="light">白色</a-radio>
+            </a-radio-group>
+            <h2>导航样式</h2>
+            <a-radio-group
+              :value="$route.query.navLayout || 'left'"
+              @change="e => handleSettingChange('navLayout', e.target.value)"
+            >
+              <a-radio value="left">左边</a-radio>
+              <a-radio value="right">右边</a-radio>
+            </a-radio-group>
+          </div>
+        </template>
+      </div>
     </a-drawer>
   </div>
 </template>
@@ -29,6 +47,9 @@ export default {
   methods: {
     onClose() {
       this.visible = false;
+    },
+    handleSettingChange(type, value) {
+      this.$router.push({ query: { ...this.$route.query, [type]: value } });
     }
   }
 };
