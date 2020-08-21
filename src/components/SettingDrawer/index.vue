67a31af2f32259e2bbe9f5041d7752b6a6b35ada
shamehub <wxk.hub@gmail.com>
2020-08-21 22:48:06 +0800

    add drawer

---
+++ src/components/SettingDrawer/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <a-drawer
+      title="Basic Drawer"
+      placement="right"
+      :closable="false"
+      :visible="visible"
+      @close="onClose"
+      width="300px"
+    >
+      <template v-slot:handle>
+        <div class="handle" @click="visible = !visible">
+          <a-icon :type="visible ? 'close' : 'setting'"></a-icon>
+        </div>
+      </template>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+    </a-drawer>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      visible: false
+    };
+  },
+  methods: {
+    onClose() {
+      this.visible = false;
+    }
+  }
+};
+</script>
+<style scoped>
+.handle {
+  position: absolute;
+  top: 240px;
+  right: 300px;
+  width: 48px;
+  height: 48px;
+  background: #1890ff;
+  color: white;
+  font-size: 20px;
+  line-height: 48px;
+  text-align: center;
+  border-radius: 3px 0 0 3px;
+}
+</style>
--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>Header</div>
+  <div class="header">Header</div>
 </template>
+<script>
+export default {};
+</script>
+<style scoped>
+.header {
+  float: right;
+}
+</style>
